b64995143ff5b18f83569046ac01b7e95e5d0027
blueisok <94949245+blueisok@users.noreply.github.com>
2022-04-08 21:33:29 +0800

    Add files via upload
    通讯录修改版（可以动态开辟空间，加入文件）
    但不知为何不可以输入中文的名字，地址等会乱码，其他的功能正常

---
+++ Project16/test_4_8.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"contact.h"
+void menu()
+{
+	printf("*************************\n");
+	printf("***1.add      2.del******\n");
+	printf("***3.search   4.modify*****\n");
+	printf("***5.show     6.sort******\n");
+	printf("***7.save     0.exit *****\n");
+	printf("*************************\n");
+}
+int main()
+{
+	int input = 0;
+	struct Contact con;
+	initcontact(&con);//初始化通讯录
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			addcontact(&con);//添加通讯录
+			break;
+		case DEL:
+			delcontact(&con);//删除通讯录
+			break;
+		case SEARCH://查找某个人的信息
+			searchcontact(&con);
+			break;
+		case MODIFY://修改某个人信息
+			modifycontact(&con);
+			break;
+		case SHOW:
+			showcontact(&con);//传地址，速度快
+			break;
+		case SORT:
+			sortcontact(&con);
+			break;
+		case SAVE:
+			savecontact(&con);//保存文件
+			break;
+		case EXIT:
+			savecontact(&con);//保存文件
+			destroycontact(&con);
+			printf("退出程序\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+
+	} while (input);
+	return 0;
+}
